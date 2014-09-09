7a14462363857149d6417b81adeb4f42b0531e13
Luis Velasco <luisvm@gmail.com>
2014-09-09 01:07:29 -0500

    Add fixes to syntax errors

--- hotpatch.sh
+++ hotpatch.sh
@@ -10,21 +10,28 @@ echo "Hotfix branch: "
 read HOTFIX_BRANCH
 
 if [ -z $HOTFIX_BRANCH ]; then
-  echo "Hotfix branch is necessary"
+  echo "FATAL: Hotfix branch is necessary"
   exit 1
 fi
 
 echo "Hotfix to branch (production): "
 read HOTFIX_TO_BRANCH
 
-if [ -z $HOTFIX_TO_BRANCH ]; then
+if [[ -z $HOTFIX_TO_BRANCH ]]; then
   HOTFIX_TO_BRANCH='production'
+  BRANCH_EXISTS=`git branch -a | grep $HOTFIX_TO_BRANCH`
+
+  if [[ -z $BRANCH_EXISTS ]]; then
+    echo "FATAL: Branch '${HOTFIX_TO_BRANCH}' doesn't exist"
+    exit 1
+  fi
 fi
 
 # make the resulting branch
 HOTFIX_RESULTING_BRANCH="${HOTFIX_TO_BRANCH}_${HOTFIX_BRANCH}"
 
 git checkout $HOTFIX_TO_BRANCH
+echo "Pulling latest code from remote/${HOTFIX_TO_BRANCH}"
 git pull
 
 # check out a local clone of the remote branch that was approved
@@ -33,15 +40,20 @@ git checkout -b $HOTFIX_RESULTING_BRANCH origin/$HOTFIX_BRANCH
 # confirm the list of chages to be patched onto production
 ANCESTOR=`git ancestor`
 
-for [ -z $CORRECT || $CORRECT == 'n' || $CORRECT == 'N' ]; do
+while [ -z $CORRECT ] || [ $CORRECT == 'n' ] || [ $CORRECT == 'N' ]; do
   git log --pretty=oneline --no-merges --first-parent $ANCESTOR..HEAD
 
-  echo "Is this what you want to hotpatch? (Y|n)"
+  echo "Is this what you want to hotpatch? (y|N)"
   read CORRECT
 
-  if ![ $CORRECT == 'y' || $CORRECT == 'Y' || -z $CORRECT ]; then
+  if [ $CORRECT == 'n' ] || [ $CORRECT == 'N' ] || [ -z $CORRECT ]; then
     echo "Ancestor commit: "
     read ANCESTOR
+
+    if [[ -z $ANCESTOR ]]; then
+      echo "FATAL: Ancestor is required"
+      exit 1
+    fi
   fi
 done
 
